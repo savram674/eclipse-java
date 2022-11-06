7d32aa2ba0e0dcca4988c0992179208142ee0e0c
savram674 <savitharamesh674@gmail.com>
2022-11-06 22:26:40 +0530

    Hello World Maven java project

---
+++ src/test/java/in/kmit/HelloWorld/AppTest.java
@@ -0,0 +1,18 @@
+package in.kmit.HelloWorld;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void AppTestDemo()
+    {
+        System.out.println("\n.....Testing App.java ......\n");
+    }
+}
